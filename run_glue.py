72a23878ec2e6a6c9a2806e895c4d8f97259150e
AlexisZam <alexiszamanis1997@gmail.com>
2021-02-26 21:49:09 +0200

    removed support for files and other tasks

--- run_glue.py
+++ run_glue.py
@@ -42,16 +42,10 @@ from transformers import (
 )
 from transformers.trainer_utils import get_last_checkpoint, is_main_process
 
-
 task_to_keys = {
-    "cola": ("sentence", None),
     "mnli": ("premise", "hypothesis"),
-    "mrpc": ("sentence1", "sentence2"),
     "qnli": ("question", "sentence"),
-    "qqp": ("question1", "question2"),
     "rte": ("sentence1", "sentence2"),
-    "sst2": ("sentence", None),
-    "stsb": ("sentence1", "sentence2"),
     "wnli": ("sentence1", "sentence2"),
 }
 
@@ -93,18 +87,6 @@ class DataTrainingArguments:
             "If False, will pad the samples dynamically when batching to the maximum length in the batch."
         },
     )
-    train_file: Optional[str] = field(
-        default=None,
-        metadata={"help": "A csv or a json file containing the training data."},
-    )
-    validation_file: Optional[str] = field(
-        default=None,
-        metadata={"help": "A csv or a json file containing the validation data."},
-    )
-    test_file: Optional[str] = field(
-        default=None,
-        metadata={"help": "A csv or a json file containing the test data."},
-    )
 
     def __post_init__(self):
         if self.task_name is not None:
@@ -114,18 +96,8 @@ class DataTrainingArguments:
                     "Unknown task, you should pick one in "
                     + ",".join(task_to_keys.keys())
                 )
-        elif self.train_file is None or self.validation_file is None:
-            raise ValueError("Need either a GLUE task or a training/validation file.")
         else:
-            train_extension = self.train_file.split(".")[-1]
-            assert train_extension in [
-                "csv",
-                "json",
-            ], "`train_file` should be a csv or a json file."
-            validation_extension = self.validation_file.split(".")[-1]
-            assert (
-                validation_extension == train_extension
-            ), "`validation_file` should have the same extension (csv or json) as `train_file`."
+            raise ValueError("Need a GLUE task.")
 
 
 @dataclass
@@ -178,7 +150,7 @@ class ModelArguments:
     )
 
 
-def main():
+if __name__ == "__main__":
     # See all possible arguments in src/transformers/training_args.py
     # or by passing the --help flag to this script.
     # We now keep distinct sets of args, for a cleaner separation of concerns.
@@ -251,66 +223,14 @@ def main():
     #
     # In distributed training, the load_dataset function guarantee that only one local process can concurrently
     # download the dataset.
-    if data_args.task_name is not None:
-        # Downloading and loading a dataset from the hub.
-        datasets = load_dataset("glue", data_args.task_name)
-    else:
-        # Loading a dataset from your local files.
-        # CSV/JSON training and evaluation files are needed.
-        data_files = {
-            "train": data_args.train_file,
-            "validation": data_args.validation_file,
-        }
-
-        # Get the test dataset: you can provide your own CSV/JSON test file (see below)
-        # when you use `do_predict` without specifying a GLUE benchmark task.
-        if training_args.do_predict:
-            if data_args.test_file is not None:
-                train_extension = data_args.train_file.split(".")[-1]
-                test_extension = data_args.test_file.split(".")[-1]
-                assert (
-                    test_extension == train_extension
-                ), "`test_file` should have the same extension (csv or json) as `train_file`."
-                data_files["test"] = data_args.test_file
-            else:
-                raise ValueError(
-                    "Need either a GLUE task or a test file for `do_predict`."
-                )
-
-        for key in data_files.keys():
-            logger.info(f"load a local file for {key}: {data_files[key]}")
-
-        if data_args.train_file.endswith(".csv"):
-            # Loading a dataset from local csv files
-            datasets = load_dataset("csv", data_files=data_files)
-        else:
-            # Loading a dataset from local json files
-            datasets = load_dataset("json", data_files=data_files)
+    # Downloading and loading a dataset from the hub.
+    datasets = load_dataset("glue", data_args.task_name)
     # See more about loading any type of standard or custom dataset at
     # https://huggingface.co/docs/datasets/loading_datasets.html.
 
     # Labels
-    if data_args.task_name is not None:
-        is_regression = data_args.task_name == "stsb"
-        if not is_regression:
-            label_list = datasets["train"].features["label"].names
-            num_labels = len(label_list)
-        else:
-            num_labels = 1
-    else:
-        # Trying to have good defaults here, don't hesitate to tweak to your needs.
-        is_regression = datasets["train"].features["label"].dtype in [
-            "float32",
-            "float64",
-        ]
-        if is_regression:
-            num_labels = 1
-        else:
-            # A useful fast method:
-            # https://huggingface.co/docs/datasets/package_reference/main_classes.html#datasets.Dataset.unique
-            label_list = datasets["train"].unique("label")
-            label_list.sort()  # Let's sort it for determinism
-            num_labels = len(label_list)
+    label_list = datasets["train"].features["label"].names
+    num_labels = len(label_list)
 
     # Load pretrained model and tokenizer
     #
@@ -345,23 +265,7 @@ def main():
     )
 
     # Preprocessing the datasets
-    if data_args.task_name is not None:
-        sentence1_key, sentence2_key = task_to_keys[data_args.task_name]
-    else:
-        # Again, we try to have some nice defaults but don't hesitate to tweak to your use case.
-        non_label_column_names = [
-            name for name in datasets["train"].column_names if name != "label"
-        ]
-        if (
-            "sentence1" in non_label_column_names
-            and "sentence2" in non_label_column_names
-        ):
-            sentence1_key, sentence2_key = "sentence1", "sentence2"
-        else:
-            if len(non_label_column_names) >= 2:
-                sentence1_key, sentence2_key = non_label_column_names[:2]
-            else:
-                sentence1_key, sentence2_key = non_label_column_names[0], None
+    sentence1_key, sentence2_key = task_to_keys[data_args.task_name]
 
     # Padding strategy
     if data_args.pad_to_max_length:
@@ -372,11 +276,7 @@ def main():
 
     # Some models have set the order of the labels to use, so let's make sure we do use it.
     label_to_id = None
-    if (
-        model.config.label2id != PretrainedConfig(num_labels=num_labels).label2id
-        and data_args.task_name is not None
-        and not is_regression
-    ):
+    if model.config.label2id != PretrainedConfig(num_labels=num_labels).label2id:
         # Some have all caps in their config, some don't.
         label_name_to_id = {k.lower(): v for k, v in model.config.label2id.items()}
         if list(sorted(label_name_to_id.keys())) == list(sorted(label_list)):
@@ -389,8 +289,6 @@ def main():
                 f"model labels: {list(sorted(label_name_to_id.keys()))}, dataset labels: {list(sorted(label_list))}."
                 "\nIgnoring the model labels as a result.",
             )
-    elif data_args.task_name is None and not is_regression:
-        label_to_id = {v: i for i, v in enumerate(label_list)}
 
     if data_args.max_seq_length > tokenizer.model_max_length:
         logger.warn(
@@ -427,18 +325,14 @@ def main():
     eval_dataset = datasets[
         "validation_matched" if data_args.task_name == "mnli" else "validation"
     ]
-    if data_args.task_name is not None or data_args.test_file is not None:
-        test_dataset = datasets[
-            "test_matched" if data_args.task_name == "mnli" else "test"
-        ]
+    test_dataset = datasets["test_matched" if data_args.task_name == "mnli" else "test"]
 
     # Log a few random samples from the training set:
     for index in random.sample(range(len(train_dataset)), 3):
         logger.info(f"Sample {index} of the training set: {train_dataset[index]}.")
 
     # Get the metric function
-    if data_args.task_name is not None:
-        metric = load_metric("glue", data_args.task_name)
+    metric = load_metric("glue", data_args.task_name)
     # TODO: When datasets metrics include regular accuracy, make an else here and remove special branch from
     # compute_metrics
 
@@ -446,16 +340,11 @@ def main():
     # predictions and label_ids field) and has to return a dictionary string to float.
     def compute_metrics(p: EvalPrediction):
         preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
-        preds = np.squeeze(preds) if is_regression else np.argmax(preds, axis=1)
-        if data_args.task_name is not None:
-            result = metric.compute(predictions=preds, references=p.label_ids)
-            if len(result) > 1:
-                result["combined_score"] = np.mean(list(result.values())).item()
-            return result
-        elif is_regression:
-            return {"mse": ((preds - p.label_ids) ** 2).mean().item()}
-        else:
-            return {"accuracy": (preds == p.label_ids).astype(np.float32).mean().item()}
+        preds = np.argmax(preds, axis=1)
+        result = metric.compute(predictions=preds, references=p.label_ids)
+        if len(result) > 1:
+            result["combined_score"] = np.mean(list(result.values())).item()
+        return result
 
     # Data collator will default to DataCollatorWithPadding, so we change it if we already did the padding.
     if data_args.pad_to_max_length:
@@ -543,11 +432,7 @@ def main():
             # Removing the `label` columns because it contains -1 and Trainer won't like that.
             test_dataset.remove_columns_("label")
             predictions = trainer.predict(test_dataset=test_dataset).predictions
-            predictions = (
-                np.squeeze(predictions)
-                if is_regression
-                else np.argmax(predictions, axis=1)
-            )
+            predictions = np.argmax(predictions, axis=1)
 
             output_test_file = os.path.join(
                 training_args.output_dir, f"test_results_{task}.txt"
@@ -557,18 +442,5 @@ def main():
                     logger.info(f"***** Test results {task} *****")
                     writer.write("index\tprediction\n")
                     for index, item in enumerate(predictions):
-                        if is_regression:
-                            writer.write(f"{index}\t{item:3.3f}\n")
-                        else:
-                            item = label_list[item]
-                            writer.write(f"{index}\t{item}\n")
-    return eval_results
-
-
-def _mp_fn(index):
-    # For xla_spawn (TPUs)
-    main()
-
-
-if __name__ == "__main__":
-    main()
+                        item = label_list[item]
+                        writer.write(f"{index}\t{item}\n")
